180b925d992cbfa35127cb89862c85d0a7cc2a62
DongWoo Kim <dongwookim@DongWooui-MacBookPro.local>
2020-03-21 00:59:47 +0900

    JSX Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Potato from './Potato'
 
 function App() {
   return (
     <div className="App"> Hello!
+    <Potato></Potato>
     </div>
   );
 }
 
 export default App;
+
+/* 리액트는 컴포넌트를 사용해서 HTML처럼 작성하려는 경우에 필요하다
+   jsx는 자바스크립트안의 HTML이다. 컴포넌트를 만들고 어떻게 사용하는지에 대한 것이다 */
--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
+// 리액트 어플리케이션은 하나의 컴포넌트만 랜더링한다
