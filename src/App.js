fc4f9340642fb1e53c4cc27c6dfa4bd15d454dcb
DongWoo Kim <dongwookim@DongWooui-MacBookPro.local>
2020-03-22 15:00:10 +0900

    style Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from "axios";
 import Movie from "./Movies";
+import "./App.css"
 
 
 class App extends React.Component{
@@ -11,19 +12,25 @@ class App extends React.Component{
 
   getMovies = async () => {
     const {data:{data:{movies}}} =  await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
-    // console.log(movies)
     this.setState({movies, isLoading: false})
-    // 원래 movies:movies 이고 뒤에 movies는 axios로 가져온 데이터들이 들어있는 movies임
   }
   async componentDidMount(){
-    // console.log("mount")
     this.getMovies()
   };
   render(){
-    // console.log("rendered")
     const {isLoading, movies} = this.state;
-    return <div>{isLoading ? "Loading.." : movies.map(movie=>{ //console.log(movie) 
-      return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />})}</div>
+    return( 
+    <section>{isLoading ? 
+      <div className="loader">
+        <span className="loader__text">"Loading.."</span>
+      </div>
+      : 
+      <div className="movies">
+        {movies.map(movie=>{return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres}/>})}
+      </div> 
+    }
+    </section>
+    )
   }
 }
 
--- src/Movies.js
+++ src/Movies.js
@@ -2,8 +2,20 @@ import React from "react";
 import ProTypes from "prop-types";
 // state를 쓰지 않으므로 function형을 쓰겠음.
 
-function Movie({id, year, title, summary, poster}){
-return <h4>{title}</h4>
+function Movie({year, title, summary, poster, genres}){
+    return (
+        <div className="movie">
+            <img src={poster} alt={title} title={title}/>
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genres">
+                     {genres.map((genre,index) => {return <li key={index} className="genres__genre">{genre}</li>})}
+                </ul>
+                <p className="movie__summary">{summary.slice(0,140)}...</p>
+            </div>
+        </div>
+    )
 }
 
 Movie.propTypes = {
@@ -11,7 +23,11 @@ Movie.propTypes = {
     year: ProTypes.number.isRequired,
     title: ProTypes.string.isRequired,
     summary: ProTypes.string.isRequired,
-    poster: ProTypes.string.isRequired
+    poster: ProTypes.string.isRequired,
+    generes: ProTypes.arrayOf(ProTypes.string).isRequired
 }
 
 export default Movie;
+
+// jsx에서는 label 태그에 들어가는 for은 htmlFor로 쓰임
+// 제너럴
