0652b1f94e987b08638ac8ffc631afaa4c567486
DongWoo Kim <dongwookim@DongWooui-MacBookPro.local>
2020-03-22 15:16:04 +0900

    Updates

---
+++ precache-manifest.02409ee59dab92f7872ba1e8a814b33d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8df5240e276f2be91d27120e52889291",
+    "url": "/Nomard_movie_app/index.html"
+  },
+  {
+    "revision": "4dfd93e133093ec4659c",
+    "url": "/Nomard_movie_app/static/css/main.28e1f9a6.chunk.css"
+  },
+  {
+    "revision": "b934bae197d104ac7943",
+    "url": "/Nomard_movie_app/static/js/2.53f89e63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Nomard_movie_app/static/js/2.53f89e63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dfd93e133093ec4659c",
+    "url": "/Nomard_movie_app/static/js/main.fd00077f.chunk.js"
+  },
+  {
+    "revision": "783dc31170400495328a",
+    "url": "/Nomard_movie_app/static/js/runtime-main.6ab656dc.js"
+  }
+]);
